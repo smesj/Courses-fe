4786ee0cbfc9ee5db68d3c558441bdf28f2ecaa1
smesj <smeschishnick@gmail.com>
2021-03-01 21:26:01 -0600

    implement enroll/unenroll conditional buttons show totals

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,37 +1,69 @@
 import { Button } from '@material-ui/core';
 import moment from 'moment';
-import React, { useEffect, useState } from 'react';
+import { UserContext } from './hoc/withUser';
+import React, { useEffect, useState, useContext } from 'react';
 import axiosClient from '../axiosClient';
 
 export const Courses = ({ user }) => {
-
-	const [thing, setCourses] = useState([]);
-
+  const { setUser } = useContext(UserContext);
+	const [courses, setCourses] = useState([]);
 	const getCourses = async () => {
 		const listcoursesResponse = await axiosClient.post('/courses/list');
 		setCourses(listcoursesResponse.data.courses)
 	}
 
+  const enrollSection = async (userId, sectionId) => {
+    const request = {
+      userId,
+      sectionId
+    }
+    const enrollResponse = await axiosClient.post('/courses/enrollUserForSection', request);
+    setUser(enrollResponse.data.user)
+  }
+
+  const unenrollSection = async (userId, sectionId) => {
+    const request = {
+      userId,
+      sectionId
+    }
+    const unenrollResponse = await axiosClient.post('/courses/unenrollUserForSection', request);
+    setUser(unenrollResponse.data.user)
+  }
+
+  const isMySection = (user, section) => {
+    if (section.users.some(el => el.id === user.id)){
+      return false;
+    }
+    return true;
+  }
+
 	useEffect(() => {
 		getCourses();
-	}, [])
+	},[user])
 
 	return (
 		<>
 			<h1>Welcome: {user.username} </h1>
 			<h2>Courses</h2>
-			{thing.map(course =>{ 
+			{courses.map(course =>{ 
 				return(
 					<React.Fragment key={course.id}>
 						<h4 >{course.name}</h4>
+            <p>{course.description}</p>
 						<ul>
 							{course.courseSections.map(section => {
 								return (
 									<li key={section.id} style={{width: '50%', display: 'flex', flexDirection: 'row', justifyContent: "space-between"}}>
 										<span>{section.nickname}: {moment(section.startDate).format("MMM Do YY")}</span>
-										<Button size="small" variant="contained" color="primary">
-											Register
-										</Button>
+                    <span>{section.users.length}</span>
+                    {isMySection(user, section) ?
+                      <Button size="small" variant="contained" color="primary" onClick={() => {enrollSection(user.id, section.id)}}>
+                        Register
+                      </Button> :
+                      <Button size="small" variant="contained" color="primary" onClick={() => {unenrollSection(user.id, section.id)}}>
+                        leave
+                      </Button>
+                    }
 									</li>
 										
 								)
